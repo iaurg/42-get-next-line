8d4e0089a5ab9c136891d36860042487340a46a7
Italo aurelio <italoaurelior@gmail.com>
2021-08-12 10:53:19 -0300

    Adding more func

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,48 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 13:44:48 by itaureli          #+#    #+#             */
-/*   Updated: 2021/08/11 23:29:38 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/08/12 10:52:47 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+ssize_t	read_line(char **holder, char **buffer, char **final_line, int fd);
+
 char	*get_next_line(int fd)
 {
 	static char	*holder = NULL;
 	char		*buffer;
-	//size_t		nbytes;
+	char		*final_line;
+	int			read_n;
 
-	// return the line that has been read
 	if (!fd || fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = malloc(BUFFER_SIZE + 1 * sizeof(char));
-	if(!buffer)
+	if (!buffer)
 		return (NULL);
-	while (read(fd, buffer, BUFFER_SIZE))
+	read_n = read_line(&holder, &buffer, &final_line, fd);
+	if (read_n <= 0)
+		return (NULL);
+	return(final_line);
+}
+
+ssize_t	read_line(char **holder, char **buffer, char **final_line, int fd)
+{
+	ssize_t		read_result;
+
+	read_result = 1;
+	*holder = "";
+	while (!ft_strchr(*buffer, '\n') && read_result)
 	{
-		printf("%s--ENDBUF--", buffer);
+		read_result = read(fd, *buffer, BUFFER_SIZE);
+		*holder = ft_strjoin(*holder, *buffer);
 	}
-	free(buffer);
-	if(!holder)
-		return (NULL);
-	return("hi");
+	printf("HOLDER:%s\n", *holder);
+	if(!final_line)
+		return (0);
+	return (read_result);
 }
 
 #include <fcntl.h>
@@ -44,6 +59,6 @@ int	main(void)
 	*/
 	int file_descriptor = open("./test.txt", O_RDONLY);
 	//printf("file_descriptor: %d \n", file_descriptor);
-	printf("STR: %s \n", get_next_line(file_descriptor));
+	printf("%s", get_next_line(file_descriptor));
 	return (0);
 }
