61cec4e232ad78045786491ef70011416d033b89
Italo aurelio <italoaurelior@gmail.com>
2021-08-15 11:09:17 -0300

    Only 42 bytes now

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 13:44:48 by itaureli          #+#    #+#             */
-/*   Updated: 2021/08/14 16:40:51 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/08/15 11:08:49 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,12 @@ char	*get_next_line(int fd)
 		holder = ft_strdup("");
 	read_fd = read_line(&holder, &buffer, &final_line, fd);
 	free_and_reset(buffer);
-	if (read_fd <= 0 && !final_line)
+	if (!final_line && read_fd <= 0)
+	{
+		// 1 leak solved
+		free_and_reset(holder);
 		return (NULL);
+	}
 	return(final_line);
 }
 
@@ -45,25 +49,30 @@ ssize_t	read_line(char **holder, char **buffer, char **final_line, int fd)
 	char		*temp_result;
 
 	read_result = 1;
+	// read_result < BUFFER_SIZE
 	while (!ft_strchr(*holder, '\n') && read_result)
 	{
 		read_result = read(fd, *buffer, BUFFER_SIZE);
 		(*buffer)[read_result] = '\0'; // Allow string work
 		temp_result = *holder; // Prepare to join
 		*holder = ft_strjoin(temp_result, *buffer);
-		free_and_reset(temp_result);
+		free(temp_result);
 	}
 	*final_line = split_line(holder);
-	if(!*final_line)
+	if(read_result == 0 && !*holder)
 		return (0);
+	/*
+	if (!*final_line)
+		free_and_reset(*final_line);
+	*/
 	return (read_result);
 }
 
 char	*split_line(char **holder)
 {
 	int		i;
-	char	*holder_copy;
 	char	*temp_holder;
+	char	*holder_copy;
 
 	i = 0;
 	while ((*holder)[i] != '\0')
@@ -75,12 +84,16 @@ char	*split_line(char **holder)
 		}
 		i++;
 	}
-	if (i == 0)
+	//printf("----%s----\n", *holder);
+
+	if (i == 0 || !*holder)
 		return (NULL);
+	//printf("****%d****\n", i);
 	temp_holder = *holder;
 	holder_copy = ft_substr(temp_holder, 0, i);
-	*holder = ft_strdup(&(*holder)[i]);
-	free_and_reset(temp_holder);
+	*holder = ft_strdup("");
+	free(temp_holder);
+	temp_holder = NULL;
 	return(holder_copy);
 }
 
