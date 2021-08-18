20b0d049f05779c6083eec62a1fcef74aa278865
Italo aurelio <italoaurelior@gmail.com>
2021-08-18 00:42:10 -0300

    Trying bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/09 22:36:01 by itaureli          #+#    #+#             */
+/*   Updated: 2021/08/18 00:39:02 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+char	*get_next_line(int fd);
+void	free_and_reset(void *var);
+ssize_t	read_line(char **holder, char **buffer, char **final_line, int fd);
+char	*split_line(char **holder);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/11 20:22:32 by itaureli          #+#    #+#             */
+/*   Updated: 2021/08/18 00:37:26 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	find_c;
+
+	find_c = c;
+	while (*s != find_c)
+	{
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return ((char *)s);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_string;
+	char	*hold_pointer;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = 0;
+	s2_len = 0;
+	while (s1[s1_len])
+		s1_len++;
+	while (s2[s2_len])
+		s2_len++;
+	new_string = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!new_string)
+		return (NULL);
+	hold_pointer = new_string;
+	while (*s1)
+		*new_string++ = *s1++;
+	while (*s2)
+		*new_string++ = *s2++;
+	*new_string = '\0';
+	return (hold_pointer);
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*ptr;
+	size_t	size;
+
+	size = 0;
+	while (str[size])
+		size++;
+	size++;
+	ptr = (char *) malloc(size * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	ft_strlcpy(ptr, str, size);
+	return ((char *) ptr);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	x;
+	size_t	counter;
+
+	x = 0;
+	if (!dst || !src)
+		return (0);
+	if (dstsize)
+	{
+		while (src[x] && x < (dstsize - 1))
+		{
+			dst[x] = src[x];
+			x++;
+		}
+		dst[x] = '\0';
+	}
+	counter = 0;
+	while (src[counter])
+		counter++;
+	return (counter);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*substring_ptr;
+	size_t			str_len;
+
+	if (!s)
+		return (NULL);
+	str_len = 0;
+	while (s[str_len])
+		str_len++;
+	if (start > str_len)
+		return (ft_strdup(""));
+	else if (start + len > str_len)
+		len = str_len - start;
+	substring_ptr = malloc((len + 1) * sizeof(char));
+	if (!substring_ptr)
+		return (NULL);
+	ft_strlcpy (substring_ptr, s + start, len + 1);
+	return (substring_ptr);
+}
